7e8c8cbb4db8dcc9c120017fb42c872b42c21b1d
Aswathy k.r <aswathy19kr@gmail.com>
2023-02-01 23:50:29 +0530

    initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  data="Your Perfect Banking Partner!"
+  inputplaceholder="Enter Account Number"
+
+  acno=''
+  // or acno:any
+
+  psw=''
+
+  userDetails:any={
+    1000:{acno:1000,username:"anu",password:"abc123",balance:0},
+    1001:{acno:1000,username:"amal",password:"abc123",balance:0},
+    1003:{acno:1000,username:"arun",password:"abc123",balance:0},
+    1004:{acno:1000,username:"akhil",password:"abc123",balance:0},
+  }
+  constructor(){}
+
+
+  login(){
+    alert("login clicked")
+  }
+  acnoChange(event:any){
+    this.acno=event.target.value
+    console.log(this.acno);
+    
+  }
+  pswrdChange(event:any){
+    this.psw=event.target.value
+    console.log(this.psw);
+    
+  }
+}
