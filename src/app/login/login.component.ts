a4ec8e70e31b1f076c3a419b2907abe1afc6f5b1
Aswathy k.r <aswathy19kr@gmail.com>
2023-02-02 07:03:27 +0530

    Third commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,10 +9,10 @@ export class LoginComponent {
   data="Your Perfect Banking Partner!"
   inputplaceholder="Enter Account Number"
 
-  acno=''
+  // acno=''
   // or acno:any
 
-  psw=''
+  // psw=''
 
   userDetails:any={
     1000:{acno:1000,username:"anu",password:"abc123",balance:0},
@@ -23,9 +23,31 @@ export class LoginComponent {
   constructor(){}
 
 
-  login(){
-    var acnum=this.acno
-    var psw=this.psw
+  // login(){
+  //   var acnum=this.acno
+  //   var psw=this.psw
+  //   var userDetails=this.userDetails
+  //   if(acnum in userDetails){
+  //     if(psw==userDetails[acnum]["password"]){
+  //       alert("login Successfull!!")
+  //     }
+  //     else{
+  //       alert("Incorrect Password!")
+  //     }
+
+  //   }
+  //   else{
+  //     alert("user not Found!!")
+  //   }
+  //   // alert("login clicked")
+  // }
+
+
+  login(a:any,b:any){
+    // console.log(a.value);
+    
+    var acnum=a.value
+    var psw=b.value
     var userDetails=this.userDetails
     if(acnum in userDetails){
       if(psw==userDetails[acnum]["password"]){
@@ -39,16 +61,16 @@ export class LoginComponent {
     else{
       alert("user not Found!!")
     }
-    // alert("login clicked")
-  }
-  acnoChange(event:any){
-    this.acno=event.target.value
-    // console.log(this.acno);
     
   }
-  pswrdChange(event:any){
-    this.psw=event.target.value
-    // console.log(this.psw);
+  // acnoChange(event:any){
+  //   this.acno=event.target.value
+  //   // console.log(this.acno);
     
-  }
+  // }
+  // pswrdChange(event:any){
+  //   this.psw=event.target.value
+  //   // console.log(this.psw);
+    
+  // }
 }
