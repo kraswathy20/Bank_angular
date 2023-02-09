7813d9f1703c49180878a1a5a3020a299e97a0e5
Aswathy k.r <aswathy19kr@gmail.com>
2023-02-10 00:37:07 +0530

    fifth commit

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+constructor(private ds:DataService){}
+
+register(){
+  let userDetails= this.ds.userDetails
+}
+}
