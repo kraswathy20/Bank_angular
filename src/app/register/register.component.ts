d3c9b4b5092c37e5a30da276e8afdecc08be588e
Aswathy k.r <aswathy19kr@gmail.com>
2023-02-15 05:02:10 +0530

    nineth commit

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -9,18 +10,26 @@ import { DataService } from '../services/data.service';
 })
 export class RegisterComponent {
 
-  uname=''
-  acno=''
-  psw=''
+  
 
-constructor(private ds:DataService,private router:Router){}
+
+constructor(private ds:DataService,private router:Router,private fb:FormBuilder){}
+
+// create reactive form of register form
+registerForm=this.fb.group({
+  acno:['',[Validators.required,Validators.pattern('[0-9]+')]],
+  uname:['',[Validators.required,Validators.pattern('[a-zA-Z]+')]],
+  psw:['',[Validators.required,Validators.pattern('[0-9a-zA-Z]+')]]
+})
 
 register(){
   
     // let userDetails=this.ds.userDetails
-    var uname=this.uname
-    var acno=this.acno
-    var psw=this.psw
+    var uname=this.registerForm.value.uname
+    var acno=this.registerForm.value.acno
+    var psw=this.registerForm.value.psw
+
+    if(this.registerForm.valid){
     const result=this.ds.register(uname,acno,psw)
 
     if(result){
@@ -31,7 +40,10 @@ register(){
       alert("User Already Exist!")
     }
     // console.log(uname,acno,psw);
-    
+  }
+  else{
+    alert("invalid Form")
+  }
 }
 
 }
